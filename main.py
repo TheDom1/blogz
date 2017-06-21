305ad6e2d5e1017a0276173383ab7ae6c625f203
TheDom1 <dominiquehallan@gmail.com>
2017-06-21 14:35:52 -0500

    Bash Moves

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import traceback
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://Build-a-Blog:dom@localhost:8889/Build-a-Blog"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
